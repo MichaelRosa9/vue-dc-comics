12194820577fcfa968ed5f3140b4212202d6be9f
MichaelRosa9 <michaelrosa9@gmail.com>
2021-05-20 22:09:29 +0200

    footer-bottom

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,153 +1,177 @@
 <template>
   <footer>
-  <div class="footer-top">
-    <div class="container clear-fix">
+    <div class="footer-top">
+      <div class="container clear-fix">
+        <div class="footer-list clear-fix">
+          <ul>
+            <li class="title">
+              DC COMICS
+            </li>
+            <li>
+              Characters
+            </li>
+            <li>
+              Comics
+            </li>
+            <li>
+              Movies
+            </li>
+            <li>
+              TV
+            </li>
+            <li>
+              Games
+            </li>
+            <li>
+              Videos
+            </li>
+            <li>
+              News
+            </li>
+          </ul>
 
-      <div class="footer-list clear-fix">
-        
-        <ul>
-          <li class="title">
-            DC COMICS
-          </li>
-          <li>
-            Characters
-          </li>
-          <li>
-            Comics
-          </li>
-          <li>
-            Movies
-          </li>
-          <li>
-            TV
-          </li>
-          <li>
-            Games
-          </li>
-          <li>
-            Videos
-          </li>
-          <li>
-            News
-          </li>
-        </ul>
+          <ul>
+            <li class="title">
+              DC
+            </li>
+            <li>
+              Terms Of Use
+            </li>
+            <li>
+              Privacy policy&#40;New&#41;
+            </li>
+            <li>
+              Ad Chioices
+            </li>
+            <li>
+              Advertising
+            </li>
+            <li>
+              Jobs
+            </li>
+            <li>
+              Subscriptions
+            </li>
+            <li>
+              Talent Workshops
+            </li>
+            <li>
+              CPSC Certificates
+            </li>
+            <li>
+              Ratings
+            </li>
+            <li>
+              Shop Help
+            </li>
+            <li>
+              Contact Us
+            </li>
+          </ul>
 
-        <ul>
-          <li class="title">
-            DC
-          </li>
-          <li>
-            Terms Of Use
-          </li>
-          <li>
-            Privacy policy&#40;New&#41;
-          </li>
-          <li>
-            Ad Chioices
-          </li>
-          <li>
-            Advertising
-          </li>
-          <li>
-            Jobs
-          </li>
-          <li>
-            Subscriptions
-          </li>
-          <li>
-            Talent Workshops
-          </li>
-          <li>
-            CPSC Certificates
-          </li>
-          <li>
-            Ratings
-          </li>
-          <li>
-            Shop Help
-          </li>
-          <li>
-            Contact Us
-          </li>
-        </ul>
+          <ul>
+            <li class="title">
+              Sites
+            </li>
+            <li>
+              DC
+            </li>
+            <li>
+              MAD Magazine
+            </li>
+            <li>
+              DC Kids
+            </li>
+            <li>
+              DC Universe
+            </li>
+            <li>
+              DC Power Visa
+            </li>
+          </ul>
 
-        <ul>
-          <li class="title">
-            Sites
-          </li>
-          <li>
-            DC
-          </li>
-          <li>
-            MAD Magazine
-          </li>
-          <li>
-            DC Kids
-          </li>
-          <li>
-            DC Universe
-          </li>
-          <li>
-            DC Power Visa
-          </li>        
-        </ul>
-
-        <ul>
-          <li class="title">
-            Shop
-          </li>
-          <li>
-            Shop DC
-          </li>
-          <li>
-            Shop DC Collectibles
-          </li>        
-        </ul>
-      </div>
-      <div class="box">
-        <img src="" alt="">
+          <ul>
+            <li class="title">
+              Shop
+            </li>
+            <li>
+              Shop DC
+            </li>
+            <li>
+              Shop DC Collectibles
+            </li>
+          </ul>
+        </div>
+        <div class="box">
+          <img src="" alt="" />
+        </div>
       </div>
     </div>
 
-  </div>
+    <div class="footer-bottom">
+      <div class="container flex-center-between">
+        <button type="button">SIGN-UP NOW!</button>
 
-  <div class="footer-bottom"></div>
+        <div class="social-media flex-center-between">
+          <h3>
+            FOLLOW US
+          </h3>
+
+          <ul class="social-links flex-center-between">
+            <li>
+              <a href="#"
+                ><img src="../assets/img/footer-facebook.png" alt=""
+              /></a>
+            </li>
+            <li>
+              <a href="#"><img src="../assets/img/footer-twitter.png" alt=""/></a>
+            </li>
+            <li>
+              <a href="#"><img src="../assets/img/footer-youtube.png" alt=""/></a>
+            </li>
+            <li>
+              <a href="#"
+                ><img src="../assets/img/footer-pinterest.png" alt=""
+              /></a>
+            </li>
+            <li>
+              <a href="#"
+                ><img src="../assets/img/footer-periscope.png" alt=""
+              /></a>
+            </li>
+          </ul>
+        </div>      
+      </div>
+    </div>
   </footer>
 </template>
 
 <script>
-
-
 export default {
-  name: 'Footer',  
-  data(){
-    return{
-      
-    }
-  }
-}
+  name: "Footer",
+  data() {
+    return {};
+  },
+};
 </script>
 
-
 <style scoped lang="scss">
-
-@import '../assets/styles/vars.scss';
+@import "../assets/styles/vars.scss";
 
 .footer-top {
-    height: 370px;
-    width: 100%;
-    background-image: url('~@/assets/img/footer-bg.jpg');
-    background-size: cover;
-
+  height: 370px;
+  width: 100%;
+  background-image: url("~@/assets/img/footer-bg.jpg");
+  background-size: cover;
 
   .footer-list {
     position: relative;
     padding-top: 20px;
-    width: 450px;    
+    width: 450px;
     float: left;
   }
 
-  ul{
+  ul {
     color: grey;
     float: left;
     padding-right: 40px;
@@ -156,12 +180,12 @@ export default {
       font-weight: 300;
       font-size: 13px;
       margin-bottom: 6px;
-      white-space:nowrap
+      white-space: nowrap;
     }
   }
-  ul:last-child {    
-      position: absolute;
-      bottom: -20px;
+  ul:last-child {
+    position: absolute;
+    bottom: -20px;
   }
   .title {
     font-weight: 700;
@@ -176,10 +200,29 @@ export default {
     width: 560px;
     float: right;
     margin-right: 25px;
-    background-image: url('~@/assets/img/dc-logo-bg.png');
+    background-image: url("~@/assets/img/dc-logo-bg.png");
     background-size: cover;
     background-position: center;
   }
-}
+} //END footer-top
+
+.footer-bottom {
+  height: 110px;
+  background-color: #2a2a2a;
+  text-transform: uppercase;
   
+  button {
+    background-color: inherit;
+    padding: 10px;
+    border: solid 2px $brand-color;
+    color: white;
+  }
+
+  h3 {
+    color: $brand-color;
+  }
+  li {
+    margin-left: 20px;
+  }
+}
 </style>
